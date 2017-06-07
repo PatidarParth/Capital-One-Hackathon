14a0323db7f72bd9ac09e95d28b81e9c8b7fb126
Clayton Dittman <claywdittman@icloud.com>
2017-06-07 16:19:26 -0400

    Created Project in Xcode

---
+++ CapitalOne Onboarding/CapitalOne OnboardingTests/CapitalOne_OnboardingTests.swift
@@ -0,0 +1,36 @@
+//
+//  CapitalOne_OnboardingTests.swift
+//  CapitalOne OnboardingTests
+//
+//  Created by Dittman, Clayton on 6/7/17.
+//  Copyright © 2017 Herro. All rights reserved.
+//
+
+import XCTest
+@testable import CapitalOne_Onboarding
+
+class CapitalOne_OnboardingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
